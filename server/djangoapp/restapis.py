a5ae88a8c7a68502f0457b9585512c574003bfd4
Jacob Jones <keneast24@gmail.com>
2026-05-07 13:32:41 -0400

    Update restapis.py

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -39,6 +39,7 @@ def analyze_review_sentiments(text):
         print(f"Unexpected {err=}, {type(err)=}")
         print("Network exception occurred")
 
+
 def post_review(data_dict):
     request_url = backend_url+"/insert_review"
     try:
@@ -47,3 +48,4 @@ def post_review(data_dict):
         return response.json()
     except:
         print("Network exception occurred")
+
